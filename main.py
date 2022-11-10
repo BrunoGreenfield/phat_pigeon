fa4bdb8441ff0c30995d7d015bcc0e0e24b442b1
Greenyexe <21greenfieldb@thomas-hardye.net>
2022-11-10 16:40:32 +0000

    no errors, no changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from sys import exit
 
 # Constants
 FRAME_RATE = 60
-HEIGHT = 720
-WIDTH = 1280
+HEIGHT = 600
+WIDTH = 1200
 
 # Classes
 class Player(pygame.sprite.Sprite):
@@ -16,22 +16,47 @@ class Player(pygame.sprite.Sprite):
 
     self.gravity = 0
 
+
   def player_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            self.gravity = -10
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w]: 
+      self.gravity = -10
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+      self.gravity = 10
 
   def apply_gravity(self):
-      self.gravity += 0.5
-      self.rect.y += self.gravity
-      if self.rect.bottom >= 720:
-          self.rect.bottom = 720
+    global game_active
+    self.gravity += 0.5
+    self.rect.y += self.gravity
+    if self.rect.bottom >= HEIGHT:
+      self.rect.bottom = HEIGHT
+    if self.rect.top <= 0:
+      game_active = False
+
+  def update(self):
+    self.player_input()
+    self.apply_gravity()
+
+
+class Obstacle(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.image = pygame.Surface((60, 60))
+    self.image.fill((0, 255, 0))
+    self.rect = self.image.get_rect(midbottom=(WIDTH, HEIGHT//2))
   
   def update(self):
-        self.player_input()
-        self.apply_gravity()
+    self.rect.x -= 2
+    if self.rect.right <= 0:
+      self.kill()
 
 # Functions
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite,obstacle,False):
+        obstacle.empty()
+        return False
+    else: return True
+
 def update_fps():
 	fps = str(int(clock.get_fps()))
 	fps_text = pixel_font.render(f'FPS: {fps}', False, pygame.Color(64,64,64))
@@ -54,12 +79,15 @@ pygame.display.set_caption("FAT PIGEON")
 clock = pygame.time.Clock()
 start_time = 0
 pixel_font = pygame.font.Font('font/pixel.ttf', 24)
-game_active = False
+game_active = True
 score = 0
 
 # Groups
 player = pygame.sprite.GroupSingle()
 player.add(Player())
+
+obstacle = pygame.sprite.Group()
+
 
 # Game loop
 while True:
@@ -76,9 +104,13 @@ while True:
     screen.blit(update_fps(), (0,0))
     player.draw(screen)
     player.update()
+
+    obstacle.draw(screen)
+    obstacle.update()
+    game_active = collision_sprite()
   else:
     screen.fill((255,255,255))
-    score_message = pixel_font.render(f'Your scored: {score}', False, (111,196,169))
+    score_message = pixel_font.render(f'Your scored: {score}', False, (255,0,0))
     score_message_rect = score_message.get_rect(center = (WIDTH/2, HEIGHT/2))
     screen.blit(score_message, score_message_rect)
 
